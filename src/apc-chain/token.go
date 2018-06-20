97a4cd783b2d988778d9e6f4ef8c3c34e86e4718
cylof22 <cylof22@gmail.com>
2018-06-20 17:16:43 +0800

    add token to user conversion

--- src/apc-chain/token.go
+++ src/apc-chain/token.go
@@ -18,6 +18,25 @@ type TokenSaleService struct {
 	Session *mgo.Session
 }
 
+const (
+	artist = iota
+	photographer
+)
+
+// UserInfo define the basic user information
+type UserInfo struct {
+	ID         string `json:"id"`
+	Address    string `json:"address"`
+	WechatID   string `json:"wechatid"`
+	TelegramID string `json:"telgramid"`
+	Profession uint32 `json:"profession"`
+	Name       string `json:"username"`
+	Password   string `json:"password"`
+	Phone      string `json:"phone"`
+	Email      string `json:"email"`
+	Portrait   string `json:"headPortraitUrl"`
+}
+
 // TokenSaleInfo define the basic information of the token sale info
 type TokenSaleInfo struct {
 	Address    string `json:"address"`
@@ -27,6 +46,27 @@ type TokenSaleInfo struct {
 	Phone      string `json:"phone"`
 	Name       string `json:"name"`
 	Profession string `json:"profession"`
+}
+
+func tokenSale2User(info TokenSaleInfo) UserInfo {
+	var user UserInfo
+	user.Address = info.Address
+	user.WechatID = info.WechatID
+	user.TelegramID = info.TelegramID
+	user.Email = info.Mail
+	user.Phone = info.Phone
+	user.Name = info.Name
+
+	switch info.Profession {
+	case "0":
+		user.Profession = artist
+	case "1":
+		user.Profession = photographer
+	}
+	// generate unique id
+	user.ID = ""
+
+	return user
 }
 
 func (svc *TokenSaleService) ServeHTTP(res http.ResponseWriter, req *http.Request) {
@@ -54,12 +94,24 @@ func (svc *TokenSaleService) ServeHTTP(res http.ResponseWriter, req *http.Reques
 	if err != nil || count != 0 {
 		res.Write([]byte("Duplicated Wallet address"))
 		res.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	err = c.Insert(info)
 	if err != nil {
 		res.Write([]byte(err.Error()))
 		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// insert the user infor
+	c = session.DB("store").C("users")
+	userInfo := tokenSale2User(info)
+	err = c.Insert(userInfo)
+	if err != nil {
+		res.Write([]byte(err.Error()))
+		res.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
 	res.WriteHeader(http.StatusOK)
